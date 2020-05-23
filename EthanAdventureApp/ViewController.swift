e0dac9e99a1f18a1565cd05edd460104795cb0f2
Toby Kreiman <tobykreiman@gmail.com>
2020-05-23 00:12:37 -0400

    Initial Commit

---
+++ EthanAdventureApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EthanAdventureApp
+//
+//  Created by Toby Kreiman on 5/23/20.
+//  Copyright © 2020 Toby Kreiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
